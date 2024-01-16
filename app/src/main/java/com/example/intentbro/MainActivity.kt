a7fbb84042b5c8f92976f128c9643c2aa11751c9
tarnishedrey <reyyasgod@gmail.com>
2024-01-16 13:53:06 +0700

    Init Commit

---
+++ app/src/main/java/com/example/intentbro/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.intentbro
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.intentbro.ui.theme.IntentbroTheme
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+
+            }
+        }
+    }
+}
+
+
+
+
